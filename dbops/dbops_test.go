46227d7411a4caa084f4857562e201804924ad7e
Eric <bojun1117@gmail.com>
2022-07-25 20:46:19 +0800

    7/25

---
+++ dbops/dbops_test.go
@@ -0,0 +1,66 @@
+package dbops
+
+import (
+	"testing"
+)
+
+func TestAddUser(t *testing.T) {
+	err := AddUserCredential("test", "test")
+	if err != nil {
+		t.Errorf("Error of AddUser: %v", err)
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	id, err := GetUserCredential("test", "test")
+	if id != 1 || err != nil {
+		t.Errorf("Error of GetUser")
+	}
+	t.Logf("user id: %d\n",id)
+}
+
+func TestAddVideoInfo(t *testing.T) {
+	err := AddNewVideo("test", "my-video")
+	if err != nil {
+		t.Errorf("Error of AddVideoInfo: %v", err)
+	}
+}
+
+func TestGetVideoInfo(t *testing.T) {
+	res, err := GetVideoInfo(1)
+	if err != nil {
+		t.Errorf("Error of GetVideoInfo: %v", err)
+	}
+	t.Logf("video id: %d\n",res.Video_id)
+}
+
+func TestDeleteVideoInfo(t *testing.T) {
+	err := DeleteVideoInfo(1, "test")
+	if err != nil {
+		t.Errorf("Error of DeleteVideoInfo: %v", err)
+	}
+}
+
+func TestAddComments(t *testing.T) {
+	err := AddNewComments(1, "test", "test")
+	if err != nil {
+		t.Errorf("Error of AddComments: %v", err)
+	}
+}
+
+func TestListComments(t *testing.T) {
+	res, err := ListComments(1)
+	if err != nil {
+		t.Errorf("Error of ListComments: %v", err)
+	}
+	for i, ele := range res {
+		t.Logf("comment: %d, %v \n", i, ele)
+	}
+}
+
+func TestDeleteComments(t *testing.T) {
+	err := DeleteCommentInfo(1, "test")
+	if err != nil {
+		t.Errorf("Error of DeleteComments: %v", err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"net/http"
+	"video-streaming/defs"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type middleWareHandler struct {
+	r *httprouter.Router
+	l *ConnLimiter
+}
+
+func NewMiddleWareHandler(r *httprouter.Router, cc int) http.Handler { //初始化handler
+	m := middleWareHandler{}
+	m.r = r
+	m.l = NewConnLimiter(cc)
+	return m
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) { //實作ServeHTTP method
+	if !m.l.GetConn() {
+		sendErrorResponse(w, defs.ErrorTooManyRequests)
+		return
+	}
+	m.r.ServeHTTP(w, r)
+	defer m.l.ReleaseConn()
+}
+
+func RegisterHandlers() *httprouter.Router { //API控制
+	router := httprouter.New()
+	router.GET("/", homehandler)
+	router.GET("/user", createUser)
+	router.POST("/user/:username", login)
+	router.GET("/user/:username", getUserInfo)
+	router.POST("/user/:username/videos", addNewVideo)
+	router.GET("/user/:username/videos", listAllVideos)
+	router.GET("/user/:username/videos/:vid", getVideo)
+	router.DELETE("/user/:username/videos/:vid", deleteVideo)
+	router.POST("/user/:username/videos/:vid/comments", postComment)
+	router.GET("/user/:username/videos/:vid/comments", showComments)
+	router.DELETE("/user/:username/videos/:vid/comments/:cid", deleteComment)
+	router.GET("/videos/:vid", streamHandler)
+	router.POST("/upload/:vid", uploadHandler)
+	/*
+		router.GET("/videos", homeHandler)
+		router.POST("/videos",search)
+		router.GET("/user", createUser)
+		router.POST("/", userInfo)
+		router.GET("/login", login)
+		router.POST("/login", loginCredential)
+		router.GET("/videos/:videoname",searchPage)
+		router.GET("/videos/:username",userVideos)
+		router.DELETE("/videos/:username",deleteVideo)
+		router.GET("/videos/:vid",VideoInfo)
+		router.POST("/videos/:vid",postComment)
+		router.DELETE("/videos/:vid",deleteComment)
+		router.GET("/videos/:username/upload",upload)
+		router.POST("/videos/:username/upload",uploadVideo)
+	*/
+	router.ServeFiles("/css/*filepath", http.Dir("./webserver/css"))
+	return router
+}
+
+func main() {
+	r := RegisterHandlers()
+	mh := NewMiddleWareHandler(r, 10)
+	http.ListenAndServe(":5050", mh)
+}
